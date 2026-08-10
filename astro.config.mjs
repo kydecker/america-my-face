77e0b5553777e8c8ca010aca505789e218efd612
Ky Decker <itsevadecker@gmail.com>
2026-08-10 17:28:30 -0400

    Use Cloudflare (#11)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from "astro/config";
-import netlify from "@astrojs/netlify";
 
 export default defineConfig({
   output: "static",
-  adapter: netlify(),
-  plugins: ["prettier-plugin-astro"],
   devToolbar: {
     enabled: false,
-  }
+  },
 });
